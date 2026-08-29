41e634e8aba43d680b19fbd9050be0a2a16e2dfc
ajohnson1952 <andrew.j.a1@gmail.com>
2026-08-29 14:54:08 -0500

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>🏈 CFB Pick&apos;em</h1>
-      <p>Site is live. Picks, odds, and leaderboards are coming next.</p>
-    </main>
-  );
-}
